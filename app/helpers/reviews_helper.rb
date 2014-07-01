03a6fd34a27bb5a3bc233ff46788068fbacd19b8
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-01 18:13:12 +0100

    Added star rating to index

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,7 @@
 module ReviewsHelper
   def star_rating(rating)
-    return rating if rating.is_a?(String)
+    return rating unless rating.respond_to?(:round)
+    # return rating if rating.is_a?(String)
     rounded_rating = rating.round
     remainder = (5 - rounded_rating)
     ('★' * rounded_rating) + ('☆' * remainder)
