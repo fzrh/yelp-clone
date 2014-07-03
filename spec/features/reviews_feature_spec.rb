ae8ec7e7dc92f4a8abf36920f80c72eaf6a1b136
Faezrah Rizalman <fwrizalman@gmail.com>
2014-07-03 15:34:17 +0100

    Added the before user create for adding review test

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -17,23 +17,33 @@ describe 'writing reviews' do
     end
   end
 
-  # context ' logged in' do
-  #   it 'should add the review to the restaurant' do
-  #     leave_review('Not great', 2)
-  #     expect(page).to have_content 'Not great (★★☆☆☆)'
-  #   end
-  # end
+  context ' logged in' do
+    before do
+      user = User.create email: 'hello@hello.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
+
+    it 'should add the review to the restaurant' do
+      leave_review('Not great', 2)
+      expect(page).to have_content 'Not great (★★☆☆☆)'
+    end
+  end
 end
 
 
 describe 'average ratings' do 
   before { Restaurant.create name: 'KFC', cuisine: 'Chicken'}
 
-  # context 'logged in' do
-  #   it 'calculates and display the average rating' do
-  #     leave_review('Poor', '2')
-  #     leave_review('Great', '4')
-  #     expect(page).to have_content "Average rating: ★★★☆☆"
-  #   end
-  # end
+  context 'logged in' do
+    before do
+      user = User.create email: 'hello@hello.com', password: '12345678', password_confirmation: '12345678'
+      login_as user
+    end
+    
+    it 'calculates and display the average rating' do
+      leave_review('Poor', '2')
+      leave_review('Great', '4')
+      expect(page).to have_content "Average rating: ★★★☆☆"
+    end
+  end
 end
